360598124737ffcde7a4147bcf30f142d8c0f35b
Monika Drabik <monikadrabik@MacBook-Pro-Monika-2.local>
2026-03-15 14:52:41 +0100

    added settings

---
+++ settings.py
@@ -0,0 +1,19 @@
+# settings.py
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @field_validator("ENVIRONMENT")
+    @classmethod
+    def validate_environment(cls, value):
+        ...  # implement me!
+        # prepare validator that will check whether the value of ENVIRONMENT is in (dev, test, prod)
+        allowed_values = ["prod", "dev", "test"]
+        if value not in allowed_values:
+            return value
+        else:
+            raise ValueError("Invalid value for ENVIRONMENT")
